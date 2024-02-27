8abf7e1c0367274e04f20ccdd37702623862a17c
EC2 Default User <ec2-user@ip-172-31-29-230.us-east-2.compute.internal>
2024-02-27 06:11:01 +0000

    api layer

--- index.js
+++ index.js
@@ -1,4 +1,81 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
 const {client, createTables, createUser, createProduct, fetchUsers, fetchProducts, createFavorite, fetchFavorites, destroyFavorite, fetchAllFavorites} = require("./db");
+
+app.use(express.json());
+
+// ROUTE - /api/users  localhost:8080/api/users
+
+app.get("/api/users", async (req, res, next)=>{
+   try{
+       const results = await fetchUsers();
+       res.status(200).send(results);
+   }
+   catch(err){
+       next(err)
+   } 
+});
+
+app.get("/api/products", async (req, res, next)=>{
+   try{
+       const results = await fetchProducts();
+       res.status(200).send(results);
+   }
+   catch(err){
+       next(err)
+   } 
+});
+
+//ROUTE - /api/users/:id/favorites
+
+app.get("/api/users/:id/favorites", async (req, res, next)=> {
+    try{
+        const id = req.params.id;
+        const results = await fetchFavorites(id);
+        res.status(200).send(results);
+    }
+    catch(err){
+        next(err)
+    }
+});
+
+app.get("/api/favorites", async (req, res, next)=> {
+    try{
+        const results = await fetchAllFavorites();
+        res.status(200).send(results);
+    }
+    catch(err){
+        next(err);
+    }
+});
+
+// ROUTE - /api/users/:userId/favorites/:id
+
+app.post("/api/users/:userId/favorites/:id", async (req, res, next)=>{
+    try{
+        const user_id = req.params.userId;
+        const product_id = req.params.id;
+        const result = await createFavorite({ user_id, product_id })
+        res.status(201).send(result);
+    }
+    catch(err){
+        next(err)
+    }
+});
+
+app.delete("/api/users/:userId/favorites/:id", async (req,res, next)=> {
+    try{
+        const id = req.params.id;
+        const user_id = req.params.userId;
+        await destroyFavorite({ id, user_id });
+        res.sendStatus(204);
+    }
+    catch(err){
+        next(err);
+    }
+});
 
 const init = async ()=> {
     console.log('connecting to database');
@@ -17,7 +94,6 @@ const init = async ()=> {
             createProduct({ name: 'shirt'}),
             createProduct({ name: 'tie'}),
         ]);
-        console.log(moe)
         const users = await fetchUsers();
         const products = await fetchProducts();
         const favorites = await Promise.all([
@@ -27,12 +103,12 @@ const init = async ()=> {
             createFavorite({ user_id: lucy.id, product_id: coat.id}),
   ]);
   const fetchedFavorites = await fetchAllFavorites();
-    console.log('before delete');
-  console.log(fetchedFavorites);
   await destroyFavorite({id:fetchedFavorites[0].id, user_id:fetchedFavorites[0].user_id});
   const fetchedFavoritesUpdated = await fetchAllFavorites();
-    console.log('after delete');
-  console.log(fetchedFavoritesUpdated);
-}
+  
+  app.listen(PORT, ()=>{
+      console.log(`server listening on PORT ${PORT}!`);
+  });
+};
 
 init();
